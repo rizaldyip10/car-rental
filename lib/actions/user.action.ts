5bd5f22b7bfdfffc9c7c645c1f97a43e78c18ffc
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-08-17 15:20:51 +0700

    fix: user action mongodb

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,13 +1,14 @@
 "use server";
 
-import User from "@/lib/models/user.model";
+import User from "@/lib/modals/user.modal";
 import { connect } from "@/lib/db";
 
 export async function createUser(user: any) {
     try {
         await connect();
         const newUser = await User.create(user);
-
+        console.log("New user created");
+        
         return JSON.parse(JSON.stringify(newUser));
     } catch (error) {
         console.log(error);
